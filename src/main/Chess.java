47265445694bb52f946296854d66781a71ead958
Zach <zthomas2323@gmail.com>
2021-11-13 10:10:06 -0500

    initial commit

---
+++ src/main/Chess.java
@@ -0,0 +1,8 @@
+package main;
+
+public class Chess {
+
+    public static void main(String [] args){
+
+    }
+}
---
+++ src/pieces/ChessPiece.java
@@ -0,0 +1,31 @@
+package pieces;
+
+import enums.PieceColor;
+
+public class ChessPiece {
+
+    private PieceColor pieceColor;
+    private int rank;
+    private String File
+
+    PieceColor getPieceColor(){
+
+    }
+    void setPieceColor(PieceColor color){
+
+    }
+
+    int getRank(){
+
+    }
+    void setRank(int rank){
+
+    }
+
+    String getFile(){
+
+    }
+    void setFile(String file){
+
+    }
+}
